aa35e694d9caf824e735d0433c97603a22c6c907
Aikay-Egwu <justaikay@gmail.com>
2018-08-24 14:41:51 +0100

    "Adding comments"

--- core/init.php
+++ core/init.php
@@ -1,8 +1,12 @@
 <?php
 require_once __DIR__  . '/../vendor/autoload.php';
+//load configuration settings
 configure_settings();
+//loads database settings
 loadDatabase();
+//loads 
 loadPaths();
+//loads the twig framework
 loadTwig();
 
 $loader = new Twig_Loader_Filesystem(VIEWS_PATH);
